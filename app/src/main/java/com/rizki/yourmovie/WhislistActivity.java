805750b47e5f1e2137e1ab205be42423838ca849
resturr <restur2411@gmail.com>
2021-07-23 22:51:59 +0700

    Whislist Page + Drawer

---
+++ app/src/main/java/com/rizki/yourmovie/WhislistActivity.java
@@ -0,0 +1,14 @@
+package com.rizki.yourmovie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WhislistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whislist);
+    }
+}
